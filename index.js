60a93c8010d82bcb0b44537b799b3a69b5277664
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-05-02 02:25:38 +0600

    get alldata in server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+// const cors = require('cors');
+
 const allData = require('./chefData.json');
+
 app.get('/', (req, res) => {
     res.send("China chef recipe house is running!!")
 })
 
+app.get('/allData', (req, res) => {
+    res.send(allData);
+})
 
 app.listen(port, () => {
     console.log(`China chef API is running port: ${port}`);
