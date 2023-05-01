1332f5e379c970068404b2cccbe453840c544e61
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-05-02 02:13:05 +0600

    added json data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const allData = require('./chefData.json');
 app.get('/', (req, res) => {
     res.send("China chef recipe house is running!!")
 })
+
 
 app.listen(port, () => {
     console.log(`China chef API is running port: ${port}`);
