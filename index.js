d9f6d906fbbd3dbac93dc6ed64d372fbfbe6f9cf
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-05-02 02:26:53 +0600

    added cors and call the cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-// const cors = require('cors');
+const cors = require('cors');
 
 const allData = require('./chefData.json');
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send("China chef recipe house is running!!")
 })
