94301351e278d03740f26c70f8cdd442ecd1a0e9
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-05-02 01:39:56 +0600

    china chef server site in running

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send("China chef recipe house is running!!")
+})
+
+app.listen(port, () => {
+    console.log(`China chef API is running port: ${port}`);
+});
